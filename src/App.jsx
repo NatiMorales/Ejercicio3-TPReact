0595233e145b7643692a3d72263f41c6706c210f
Natalia Morales <natii.vmorales98@gmail.com>
2024-02-12 20:53:10 -0300

    se limpio la estructura del proyecto, se agregaron las dependecies de bootstrap y react-bootstrap y se agrego el codigo base del ejercicio 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import TituloPrincipal from './components/TituloPrincipal';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+  return (
+    <section>
+      <TituloPrincipal></TituloPrincipal>
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/TituloPrincipal.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const TituloPrincipal = () => {
+    return (
+    <Container fluid>
+      <h1>Hello World!</h1>
+    </Container>
+    );
+};
+
+export default TituloPrincipal;
